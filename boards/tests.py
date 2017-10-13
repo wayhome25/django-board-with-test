b45e1d9f5e7004b12fcccf20628496b8cb394f10
Lee HyunJoo <wayhome250@gmail.com>
2017-10-14 01:28:36 +0900

    Home 뷰 테스트 추가

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls.base import resolve
+from django.urls.base import reverse
 
-# Create your tests here.
+from boards.views import home
+
+
+class HomeTest(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')  # return : '/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')  # return : ResolverMatch(func=boards.views.home, args=(), kwargs={}, url_name=home, app_names=[], namespaces=[])
+        self.assertEqual(view.func, home)
