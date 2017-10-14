09897cf7d68afde7b978fb54664ff6038955d0e0
Lee HyunJoo <wayhome250@gmail.com>
2017-10-14 14:50:38 +0900

    url 관련 테스트추가

--- boards/tests.py
+++ boards/tests.py
@@ -8,6 +8,11 @@ from boards.views import home
 
 
 class HomeTest(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board <3')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         url = reverse('home')  # return : '/'
         response = self.client.get(url)
@@ -17,10 +22,14 @@ class HomeTest(TestCase):
         view = resolve('/')  # return : ResolverMatch(func=boards.views.home, args=(), kwargs={}, url_name=home, app_names=[], namespaces=[])
         self.assertEqual(view.func, home)
 
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{}"'.format(board_topics_url))
+
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
-        Board.objects.create(name='Django', description='Django board <3')
+      Board.objects.create(name='Django', description='Django board <3')
 
     def test_board_topics_view_success_status_code(self):
         url = reverse('board_topics', kwargs={'pk': 1})
@@ -35,3 +44,11 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{}"'.format(homepage_url))
+
+
