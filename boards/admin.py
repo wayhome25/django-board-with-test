1d7c42c5d9a75e8547f4db4d62bbc4240bd75bf9
Lee HyunJoo <wayhome250@gmail.com>
2017-10-15 22:08:00 +0900

    new_topic 뷰 추가

--- boards/admin.py
+++ boards/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from boards.models import Board
+from boards.models import Board, Topic, Post
 
 
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
     list_display = ['name', 'description']
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['board', 'subject', 'starter', 'last_updated']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['message', 'topic', 'created_by']
--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 from boards.models import Board
+from boards.models import Post
+from boards.models import Topic
 
 
 def home(request):
@@ -13,3 +17,29 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  # TODO: get the currently logged in user
+
+
+        topic = Topic.objects.create(
+                subject=subject,
+                board=board,
+                starter=user
+        )
+
+        post= Post.objects.create(
+                message=message,
+                topic=topic,
+                created_by=user
+        )
+        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+
+    return render(request, 'new_topic.html', {'board': board})
