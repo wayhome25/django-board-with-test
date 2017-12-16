867fff9343b10f8ec9d0ed9e607ac863eb8eeafb
Lee HyunJoo <wayhome250@gmail.com>
2017-12-16 15:45:13 +0900

    로그아웃 기능 추가

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 
 from accounts import views as accounts_views
 from boards import views
@@ -23,6 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
