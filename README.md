7b770405884eaafa2fa488fe693eb98f15ca00ed
Lee HyunJoo <wayhome250@gmail.com>
2017-12-16 14:15:14 +0900

    회원가입 기능 추가

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 - [A Complete Beginner's Guide to Django](https://simpleisbetterthancomplex.com/series/beginners-guide/1.11/)를 참고하여 기본적인 게시판 구축과 함께 테스트 코드를 작성하는 연습을 진행한다.
 
 - 테스트와 관련된 툴을 적용해본다.
-    - travis ci
+    - travis ci  [![Build Status](https://travis-ci.org/wayhome25/django-board-with-test.svg?branch=master)](https://travis-ci.org/wayhome25/django-board-with-test)
     - codecov
     - flaek8
     - git pre-commit hook
---
+++ accounts/tests.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls.base import resolve
+from django.urls.base import reverse
+
+from accounts.views import signup
+
+
+class SignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')  # 실제 url으로 부터 연결되는 view function 관련 정보를 담은 ResolverMatch 객체를 리턴
+        self.assertEqual(view.func, signup)
+        self.assertEqual(view.view_name, 'signup')
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessfulSignUpTest(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'john',
+            'password1': 'asdf1234',
+            'password2': 'asdf1234'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_회원가입_성공후_리다이랙트(self):
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_회원가입_성공후_유저레코드생성(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_회원가입_성공후_유저로그인(self):
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+
+class InvalidSignUpTestCase(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {})
+
+    def test_signup_status_code(self):
+        """invalid form submission should return to the same page"""
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
